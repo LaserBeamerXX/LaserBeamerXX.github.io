10bb6180436fd3c3b1a2bf66f9469da836a0bc17
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-17 11:21:10 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add an event listener to the "Start Game" button
+document.getElementById("start-button").addEventListener("click", function () {
+    // You can add code here to transition to the game screen or load game assets.
+    // For a simple example, you can redirect to another page:
+    window.location.href = "game.html";
+});
