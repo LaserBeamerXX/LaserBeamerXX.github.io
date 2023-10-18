b81ba2a97984b342df873a4304a9c79528dea8a3
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-17 21:19:12 -0400

    Update reforestation.js

--- reforestation.js
+++ reforestation.js
@@ -1,7 +1,23 @@
 // Store references to the game screen and inventory
 const gameScreen = document.getElementById("game-screen");
 const inventory = document.getElementById("inventory");
-let treeToClone = null; // Initialize as null
+const backgroundImage = document.getElementById("background-image");
+
+// Function to change the background image based on screen size
+function changeBackgroundImage() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+        backgroundImage.src = "small-screen-background.jpg";
+    } else {
+        backgroundImage.src = "valley-image.jpg";
+    }
+}
+
+// Initial background image
+changeBackgroundImage();
+
+// Event listener for window resize to dynamically change the background image
+window.addEventListener("resize", changeBackgroundImage);
 
 // Event listener for adding trees to the inventory
 gameScreen.addEventListener("click", (event) => {
