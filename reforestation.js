8d134cbe27d79c3c34c22635fb879f5194ae3694
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-18 19:16:58 -0400

    Update reforestation.js

--- reforestation.js
+++ reforestation.js
@@ -2,50 +2,35 @@
 const gameScreen = document.getElementById("game-screen");
 const inventory = document.getElementById("inventory");
 let treeToClone = null; // Initialize as null
-
-// Define variables for tracking the number of planted trees and the required number to progress
-let plantedTrees = 0;
-const requiredTreesToProgress = 10; // Set the required number of trees
-
-// Function to update the tree count display
-function updateTreeCount() {
-    const treePlantedCount = document.getElementById("tree-planted");
-    if (treePlantedCount) {
-        treePlantedCount.textContent = plantedTrees;
-    }
-}
-
-// Function to check if the player can progress to the next level
-function checkProgress() {
-    if (plantedTrees >= requiredTreesToProgress) {
-        alert("You've planted enough trees to progress to the next level!");
-
-        // Redirect to the next level if available
-        window.location.href = "next-level.html";
-    }
-    updateTreeCount(); // Update the tree count display
-}
-
 // Event listener for adding trees to the inventory
 gameScreen.addEventListener("click", (event) => {
     if (treeToClone) {
-        // ...
+        // Create a new tree image to add to the game screen
+        const clonedTree = new Image();
+        clonedTree.src = treeToClone.src;
+        clonedTree.style.position = "absolute";
 
-        // Increment the plantedTrees counter
-        plantedTrees++;
+        // Calculate the position to center the tree image at the cursor
+        const offsetX = event.clientX - gameScreen.getBoundingClientRect().left - (clonedTree.width / 2);
+        const offsetY = event.clientY - gameScreen.getBoundingClientRect().top - (clonedTree.height / 2);
+        clonedTree.style.left = offsetX + "px";
+        clonedTree.style.top = offsetY + "px";
 
-        // Check if the player can progress to the next level
-        checkProgress();
+        // Add the cloned tree image to the game screen
+        gameScreen.appendChild(clonedTree);
     }
-    updateTreeCount(); // Update the tree count display
 });
 
 // Event listener for selecting a tree to clone
 inventory.addEventListener("click", (event) => {
     if (event.target.tagName === "IMG") {
-        // ...
+        // Toggle the selected class for visual indication
+        if (event.target.classList.contains("selected")) {
+            event.target.classList.remove("selected");
+            treeToClone = null; // Clear the selection
+        } else {
+            event.target.classList.add("selected");
+            treeToClone = event.target; // Store the selected tree for cloning
+        }
     }
 });
-
-// Initial tree count display
-updateTreeCount(); 
