7c0c4d208ab244a0854f655a587a3630c50b8752
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-18 18:02:50 -0400

    Update reforestation.js

--- reforestation.js
+++ reforestation.js
@@ -7,12 +7,43 @@ let treeToClone = null; // Initialize as null
 let plantedTrees = 0;
 const requiredTreesToProgress = 10; // Set the required number of trees
 
+// Define an array of levels
+const levels = [
+    { requiredTrees: 10, message: "You've planted enough trees to progress to the next level!", nextLevel: "Eco-City Sustainability Challenge" },
+    { requiredTrees: 15, message: "Great job! You can move on to the next level!" }
+];
+
+// Initialize the current level
+let currentLevel = 0;
+
+// Function to update the tree count display
+function updateTreeCount() {
+    const treeCountDisplay = document.getElementById("tree-count");
+    if (treeCountDisplay) {
+        treeCountDisplay.textContent = `Trees Planted: ${plantedTrees}`;
+    }
+}
+
 // Function to check if the player can progress to the next level
 function checkProgress() {
-    if (plantedTrees >= requiredTreesToProgress) {
-        alert("You've planted enough trees to progress to the next level!");
-        // Add logic to move to the next level here
+    if (plantedTrees >= levels[currentLevel].requiredTrees) {
+        alert(levels[currentLevel].message);
+
+        // Move to the next level
+        currentLevel++;
+
+        // Reset the plantedTrees count
+        plantedTrees = 0;
+
+        // Redirect to the next level if available
+        if (currentLevel < levels.length) {
+            // Example: You can redirect the player to a new page for the next level
+            window.location.href = `${levels[currentLevel].nextLevel}.html`;
+        } else {
+            alert("Congratulations! You've completed all levels.");
+        }
     }
+    updateTreeCount(); // Update the tree count display
 }
 
 // Event listener for adding trees to the inventory
@@ -38,6 +69,7 @@ gameScreen.addEventListener("click", (event) => {
         // Check if the player can progress to the next level
         checkProgress();
     }
+    updateTreeCount(); // Update the tree count display
 });
 
 // Event listener for selecting a tree to clone
@@ -53,3 +85,6 @@ inventory.addEventListener("click", (event) => {
         }
     }
 });
+
+// Initial tree count display
+updateTreeCount();
