ce779cf3d5cd369df83ab5e482d688a77cbf5a9f
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-18 08:11:01 -0400

    Update reforestation.js

--- reforestation.js
+++ reforestation.js
@@ -18,8 +18,8 @@ gameScreen.addEventListener("click", (event) => {
         clonedTree.style.top = offsetY + "px";
 
         // Make the cloned tree smaller
-        clonedTree.style.width = "50px"; // Adjust the size as needed
-        clonedTree.style.height = "50px"; // Adjust the size as needed
+        clonedTree.style.width = "20px"; // Adjust the size as needed
+        clonedTree.style.height = "20px"; // Adjust the size as needed
 
         // Add the cloned tree image to the game screen
         gameScreen.appendChild(clonedTree);
