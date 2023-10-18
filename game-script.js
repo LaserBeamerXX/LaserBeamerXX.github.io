185834059240e3209204c4cc57c1131b1c7012e6
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-17 20:18:02 -0400

    Update game-script.js

--- game-script.js
+++ game-script.js
@@ -1,22 +1,24 @@
 // Store references to the game screen and inventory
 const gameScreen = document.getElementById("game-screen");
 const inventory = document.getElementById("inventory");
+let treeToClone = null; // Initialize as null
 
 // Event listener for adding trees to the inventory
 gameScreen.addEventListener("click", (event) => {
+    if (treeToClone) {
+        // Clone the selected tree image and add it to the game screen
+        const clonedTree = treeToClone.cloneNode(true);
+        clonedTree.style.position = "absolute";
+        clonedTree.style.left = `${event.clientX - gameScreen.getBoundingClientRect().left}px`;
+        clonedTree.style.top = `${event.clientY - gameScreen.getBoundingClientRect().top}px`;
+        gameScreen.appendChild(clonedTree);
+    }
+});
+
+// Event listener for selecting a tree to clone
+inventory.addEventListener("click", (event) => {
     if (event.target.tagName === "IMG") {
-        // Clone the tree image and add it to the inventory
-        const treeImage = event.target.cloneNode(true);
-        inventory.appendChild(treeImage);
-        // Add a click event listener to the tree image in the inventory
-        treeImage.addEventListener("click", () => {
-            // Clone the tree image when clicked in the inventory
-            const clonedTree = treeImage.cloneNode(true);
-            // Position the cloned tree at the point where the user clicked on the game screen
-            clonedTree.style.position = "absolute";
-            clonedTree.style.left = `${event.clientX - gameScreen.getBoundingClientRect().left}px`;
-            clonedTree.style.top = `${event.clientY - gameScreen.getBoundingClientRect().top}px`;
-            gameScreen.appendChild(clonedTree);
-        });
+        // Store the selected tree for cloning
+        treeToClone = event.target;
     }
 });
