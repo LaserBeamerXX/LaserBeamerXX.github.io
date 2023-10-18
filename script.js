31067d9d2f6e946be11050d330b1f6f1ead1c32d
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-18 16:57:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 document.getElementById("start-button").addEventListener("click", function () {
     // You can add code here to transition to the game screen or load game assets.
     // For a simple example, you can redirect to another page:
-    window.location.href = "game.html";
+    window.location.href = " reforestation.html";
 });
