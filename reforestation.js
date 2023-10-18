dba306e6bbd8582c5e136568fc4fea9efab73192
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-18 08:29:02 -0400

    Update reforestation.js

--- reforestation.js
+++ reforestation.js
@@ -2,7 +2,6 @@
 const gameScreen = document.getElementById("game-screen");
 const inventory = document.getElementById("inventory");
 let treeToClone = null; // Initialize as null
-
 // Event listener for adding trees to the inventory
 gameScreen.addEventListener("click", (event) => {
     if (treeToClone) {
@@ -10,21 +9,20 @@ gameScreen.addEventListener("click", (event) => {
         const clonedTree = new Image();
         clonedTree.src = treeToClone.src;
         clonedTree.style.position = "absolute";
-
         // Calculate the position to center the tree image at the cursor
         const offsetX = event.clientX - gameScreen.getBoundingClientRect().left - (clonedTree.width / 2);
         const offsetY = event.clientY - gameScreen.getBoundingClientRect().top - (clonedTree.height / 2);
         clonedTree.style.left = offsetX + "px";
         clonedTree.style.top = offsetY + "px";
 
-        // Make the cloned tree smaller by setting its width and height directly
-        clonedTree.style.cssText = "width: 50px; height: 50px"; // Adjust the size as needed
+        // Make the cloned tree smaller
+        clonedTree.style.width = "20px"; // Adjust the size as needed
+        clonedTree.style.height = "20px"; // Adjust the size as needed
 
         // Add the cloned tree image to the game screen
         gameScreen.appendChild(clonedTree);
     }
 });
-
 // Event listener for selecting a tree to clone
 inventory.addEventListener("click", (event) => {
     if (event.target.tagName === "IMG") {
