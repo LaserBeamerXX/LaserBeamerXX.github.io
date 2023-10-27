dbdbf2dcccaaeab3e0afc7b66d9a89b80afeecbb
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-27 07:53:01 -0400

    Update climate-crusade.js

--- climate-crusade.js
+++ climate-crusade.js
@@ -1,4 +1,3 @@
-
 // JavaScript code for the Energy Consumption Dashboard
 const dashboard = document.getElementById("dashboard");
 const currentUsage = dashboard.querySelector("p:nth-of-type(1)");
@@ -14,15 +13,18 @@ updateDashboard();
 // Function to update the dashboard
 function updateDashboard() {
     currentUsage.textContent = `Current Energy Usage: ${energyUsage} kWh`;
-    potentialSavings.textContent = `Potential Energy Savings: ${energySavings}%`;
+    potentialSavings.textContent = `Potential Energy Savings: ${energySavings.toFixed(2)}%`; // Ensure two decimal places
 }
 
 // Function to handle the LED bulb upgrade
 function upgradeToLED() {
     // Calculate energy savings
-    energySavings += 30;
+    const savingsPercentage = 30; // Adjust as needed
+    energySavings += savingsPercentage;
+    
     // Update current energy usage
-    energyUsage = Math.round(energyUsage * (1 - energySavings / 100));
+    energyUsage = Math.round(energyUsage * (1 - savingsPercentage / 100));
+    
     // Update the dashboard
     updateDashboard();
 }
@@ -30,7 +32,3 @@ function upgradeToLED() {
 // Add an event listener to the LED upgrade button
 const ledUpgradeButton = document.getElementById("upgrade1");
 ledUpgradeButton.addEventListener("click", upgradeToLED);
-
-
-// Add Bill Nye's educational content and guidance as needed
-// For example, you can have Bill Nye provide information on the impact of energy-efficient upgrades.
