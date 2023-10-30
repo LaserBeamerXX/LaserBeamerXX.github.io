a7349e76b02ad233eba5a287a8d394d524a8929f
LaserBeamerXX <51236552+LaserBeamerXX@users.noreply.github.com>
2023-10-30 17:19:55 -0400

    Update ocean.js

--- ocean.js
+++ ocean.js
@@ -1,21 +1,20 @@
 const submarine = document.getElementById('submarine');
 const submarineImg = document.getElementById('submarine-img');
 const trash = document.getElementById('trash');
-const trashCounter = document.getElementById('trash-counter'); // Create an element to display the collected trash count
+const trashCounter = document.getElementById('trash-counter');
 
 let submarineX = 50;
 let submarineY = 50;
 let trashX = 200;
 let trashY = 200;
-let collectedTrash = 0; // Initialize the collected trash count
-const trashTarget = 10; // Set the target number of trash items to collect
+let collectedTrash = 0;
+const trashTarget = 10;
 
 submarine.style.left = submarineX + 'px';
 submarine.style.top = submarineY + 'px';
 trash.style.left = trashX + 'px';
 trash.style.top = trashY + 'px';
 
-// Display the initial collected trash count
 trashCounter.textContent = 'Collected: ' + collectedTrash + '/' + trashTarget;
 
 const speed = 10;
@@ -43,7 +42,7 @@ function moveSubmersible(e) {
         case 'ArrowRight':
         case 'd':
             moveX = speed;
-            submarineImg.style.transform = 'scaleX(1)';
+            submarineImg.style.transform = 'scaleX(1)'; // Reset the transformation when moving right
             break;
     }
 
@@ -71,11 +70,10 @@ function checkTrashCollection() {
         trash.style.left = trashX + 'px';
         trash.style.top = trashY + 'px';
 
-        collectedTrash++; // Increase the collected trash count
-        trashCounter.textContent = 'Collected: ' + collectedTrash + '/' + trashTarget; // Update the displayed count
+        collectedTrash++;
+        trashCounter.textContent = 'Collected: ' + collectedTrash + '/' + trashTarget;
 
         if (collectedTrash >= trashTarget) {
-            // When the target is reached, display a message or trigger level completion
             alert('Level Complete! You collected ' + collectedTrash + ' trash items.');
         }
     }
