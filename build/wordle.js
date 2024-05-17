f06a4cb9abacc9d53eed9ba72bbf44df556170d2
TonyKillam <51236552+TonyKillam@users.noreply.github.com>
2024-05-17 08:47:21 -0400

    Update wordle.js

--- build/wordle.js
+++ build/wordle.js
@@ -6,3 +6,17 @@ var row = 0;  //current guess (attempt #)
 var col = 0; //current letter for that attempt
 
 var gameOver = false;
+var word = "SQUID";       // words = ["Pescar", "Navdad"]
+
+
+window.onload = function(){
+    initialize();
+}
+
+
+function initialize() {
+    // Create the game board
+  for (let r = 0; r<height; r++) {
+      for (let c=0)
+  }
+}
